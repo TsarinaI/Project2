9710eb8042f1a9f23cc092f218842ea724b59306
ScrantonPete <scrantonpete@yahoo.com>
2019-08-12 20:00:02 -0400

    petfinder api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 var db = require("../models");
 
 module.exports = function(app) {
@@ -14,6 +15,92 @@ module.exports = function(app) {
     db.Pet.create(req.body).then(function(pet) {
       res.json(pet);
     });
+  });
+
+  app.post("/", function(req, res) {
+    console.log(JSON.stringify(req.body));
+    function logToDOM(message, dest) {
+      $(dest).html(message);
+    }
+    tokenCall();
+
+    function tokenCall() {
+      // var apiKey = "PnZY06w1n6dNWc5KJHLrSprmEkjOTubBodspYsP2MU2JDdsOLd";
+      // var apiSecret = "TBM3WWHF7Z975GlxRiNI3iyyyIqmu23f6YShtcr5";
+      var apiKey = process.env.API_KEY;
+      var apiSecret = process.env.API_SECRET;
+
+      var petFinderUrl = `https://api.petfinder.com/v2/oauth2/token`;
+      const proxyUrl = "https://shielded-hamlet-43668.herokuapp.com/";
+
+      $.ajax({
+        type: "POST",
+        url: proxyUrl + petFinderUrl,
+        data: {
+          grant_type: "client_credentials",
+          client_id: apiKey,
+          client_secret: apiSecret
+        }
+      })
+        .done(response => {
+          console.log(response);
+          console.log(response.access_token);
+          var bearerToken = response.access_token;
+
+          const message = JSON.stringify(response);
+          const container = document.querySelector(".with-proxy");
+          logToDOM(message, container);
+          apiCall(bearerToken);
+        })
+        .catch(error => {
+          console.error(error);
+          const message = JSON.stringify(error);
+          const container = document.querySelector(".with-proxy");
+          logToDOM(message, container);
+        });
+    }
+
+    function apiCall(bearerToken) {
+      var bearerToken = bearerToken;
+      var petFinderUrl = `http://api.petfinder.com/v2/animals?type=dog&random`;
+      const proxyUrl = "https://shielded-hamlet-43668.herokuapp.com/";
+
+      $.ajax({
+        url: proxyUrl + petFinderUrl,
+        headers: {
+          authorization: "Bearer " + bearerToken
+        }
+      })
+        .done(response => {
+          console.log(response);
+          console.log(response.animals[0].name);
+          $("#petDateName").append(JSON.stringify(response.animals[0].name));
+
+          console.log(response.animals[0].species);
+          $("#petDateSpecies").append(
+            JSON.stringify(response.animals[0].species)
+          );
+
+          // console.log(response.animals[0].photos[0].medium);
+          console.log(response.animals[0].url);
+          $("#petfinderURL").append(JSON.stringify(response.animals[0].url));
+
+          console.log(response.animals[0].description);
+          $("#petDateDescription").append(
+            JSON.stringify(response.animals[0].description)
+          );
+
+          const message = JSON.stringify(response);
+          const container = document.querySelector(".with-proxy");
+          logToDOM(message, container);
+        })
+        .catch(error => {
+          console.error(error);
+          const message = JSON.stringify(error);
+          const container = document.querySelector(".with-proxy");
+          logToDOM(message, container);
+        });
+    }
   });
 
   // Delete an example by id
